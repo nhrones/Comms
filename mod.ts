8d5db31fe952e0d329fa7b15534681c758e9d366
NDH <nick@ndhrones.com>
2022-06-11 17:31:18 -0400

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-// test
+export * as signalling from './signaling.ts'
 export * as rtcConnection from './rtcConnection.ts'
 export { Event } from './events.ts'
 export { initPeers, callee, registerPeer } from './peers.ts'
